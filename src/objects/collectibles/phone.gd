5ae2b20fcfa09a50ab2714783c0dc39b7646fcf7
VictorSantos6 <victor.santos6@upr.edu>
2026-03-29 17:33:05 -0400

    Rename "extinguisher" node to "Phone" in introduction scene and implement phone collectible functionality

--- src/objects/collectibles/phone.gd
+++ src/objects/collectibles/phone.gd
@@ -1,1 +1,15 @@
 extends Area2D
+
+const TUTORIAL_SCENE_PATH := "res://scenes/levels/tutorial.tscn"
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	if not body_can_pickup(_body):
+		return
+
+	set_deferred("monitoring", false)
+	get_tree().change_scene_to_file(TUTORIAL_SCENE_PATH)
+
+
+func body_can_pickup(body: Node2D) -> bool:
+	return body != null and body is CharacterBody2D
