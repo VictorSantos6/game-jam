cf74ca7f4e5919edaaca29493c55a8f859d4b3fa
Gian-MR <gian.miranda1@upr.edu>
2026-03-29 23:04:31 -0400

    fix BOOOM

--- src/core/killzone.gd
+++ src/core/killzone.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body and body.has_method("lose_life"):
-		body.lose_life()
+		body.lose_life(true)
 		return
 
 	Engine.time_scale = 0.5
